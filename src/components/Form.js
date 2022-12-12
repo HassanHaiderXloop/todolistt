3ebd6fc572ac586b6459b9f710b8006d4ed70598
Syed Hassan Haider Zaidi_2211-005-KHI-CND <119580846+HassanHaiderXloop@users.noreply.github.com>
2022-12-12 17:54:21 +0500

    code updated

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,22 @@
 import React, {useEffect} from 'react'
 import { v4 as uuidv4 } from "uuid";
 
-const Forms = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
-    const onInputChange = (event) => {
-        setInput(event.target.value);
-    };
+let zinput="";
+
+const Forms = ({  setInput, todos, setTodos, editTodo, setEditTodo }) => {
     const updateTodo = (title,id,completed)=>{
-        const newTodo = todos.map((todo)=>{
+        const newTodo = todos.map((todo)=>
             todo.id === id ? {title,id,completed} : todo
-        })
+        )
 
         setTodos(newTodo);
         setEditTodo("");
     };
+
+    const onInputChange = (event) => {
+        zinput = event.target.value;
+    };
+   
     useEffect(() =>{
         if(editTodo){
             setInput(editTodo.title)
@@ -22,11 +26,277 @@ const Forms = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
     })
     const onFormSubmit = (event) => {
         event.preventDefault();
+
         if(!editTodo){
-            setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
+            setTodos([...todos, { id: uuidv4(), title: zinput, completed: false }]);
+            console.log(zinput);
         setInput('');
+        }else{import React, {useEffect} from 'react'
+2
+import { v4 as uuidv4 } from "uuid";
+3
+​
+4
+const Forms = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+5
+    const onInputChange = (event) => {
+6
+        setInput(event.target.value);
+7
+    };
+8
+    const updateTodo = (title,id,completed)=>{
+9
+        const newTodo = todos.map((todo)=>{
+10
+            todo.id === id ? {title,id,completed} : todo
+11
+        })
+12
+​
+13
+        setTodos(newTodo);
+14
+        setEditTodo("");
+15
+    };
+16
+    useEffect(() =>{
+17
+        if(editTodo){
+18
+            setInput(editTodo.title)
+19
         }else{
+20import React, {useEffect} from 'react'
+2
+import { v4 as uuidv4 } from "uuid";
+3
+​
+4
+const Forms = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+5
+    const onInputChange = (event) => {
+6
+        setInput(event.target.value);
+7
+    };
+8
+    const updateTodo = (title,id,completed)=>{
+9
+        const newTodo = todos.map((todo)=>{import React, {useEffect} from 'react'
+2
+import { v4 as uuidv4 } from "uuid";
+3
+​
+4
+const Forms = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+5
+    const onInputChange = (event) => {
+6
+        setInput(event.target.value);
+7
+    };
+8
+    const updateTodo = (title,id,completed)=>{
+9
+        const newTodo = todos.map((todo)=>{
+10
+            todo.id === id ? {title,id,completed} : todo
+11
+        })
+12
+​
+13
+        setTodos(newTodo);
+14
+        setEditTodo("");
+15
+    };
+16
+    useEffect(() =>{
+17
+        if(editTodo){
+18
+            setInput(editTodo.title)
+19
+        }else{
+20
+            setInput('')
+21
+        }
+22
+    })
+23
+    const onFormSubmit = (event) => {
+24
+        event.preventDefault();
+25
+        if(!editTodo){
+26
+            setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
+27
+        setInput('');
+28
+        }else{
+29
             updateTodo(input,editTodo.id, editTodo.completed)
+30
+        }
+31
+        
+32
+​
+33
+    };
+34
+    return (
+35
+        <form onSubmit={onFormSubmit}>
+36
+            <input type="text"
+37
+                placeholder='Enter a Todos.....'
+38
+                className='task-input'
+39
+                value={input}
+40
+                required
+41
+                onChange={onInputChange}
+42
+            />
+43
+            <button className='button-add' type='submit'>Add</button>
+44
+        </form>
+45
+    )
+10
+            todo.id === id ? {title,id,completed} : todo
+11
+        })
+12
+​
+13
+        setTodos(newTodo);
+14
+        setEditTodo("");
+15
+    };
+16
+    useEffect(() =>{
+17
+        if(editTodo){
+18
+            setInput(editTodo.title)
+19
+        }else{
+20
+            setInput('')
+21
+        }
+22
+    })
+23
+    const onFormSubmit = (event) => {
+24
+        event.preventDefault();
+25
+        if(!editTodo){
+26
+            setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
+27
+        setInput('');
+28
+        }else{
+29
+            updateTodo(input,editTodo.id, editTodo.completed)
+30
+        }
+31
+        
+32
+​
+33
+    };
+34
+    return (
+35
+        <form onSubmit={onFormSubmit}>
+36
+            <input type="text"
+37
+                placeholder='Enter a Todos.....'
+38
+                className='task-input'
+39
+                value={input}
+40
+                required
+41
+                onChange={onInputChange}
+42
+            />
+43
+            <button className='button-add' type='submit'>Add</button>
+44
+        </form>
+45
+    )
+            setInput('')
+21
+        }
+22
+    })
+23
+    const onFormSubmit = (event) => {
+24
+        event.preventDefault();
+25
+        if(!editTodo){
+26
+            setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
+27
+        setInput('');
+28
+        }else{
+29
+            updateTodo(input,editTodo.id, editTodo.completed)
+30
+        }
+31
+        
+32
+​
+33
+    };
+34
+    return (
+35
+        <form onSubmit={onFormSubmit}>
+36
+            <input type="text"
+37
+                placeholder='Enter a Todos.....'
+38
+                className='task-input'
+39
+                value={input}
+40
+                required
+41
+                onChange={onInputChange}
+42
+            />
+43
+            <button className='button-add' type='submit'>Add</button>
+44
+        </form>
+45
+    )
+            updateTodo(zinput,editTodo.id, editTodo.completed)
         }
         
 
@@ -36,7 +306,7 @@ const Forms = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
             <input type="text"
                 placeholder='Enter a Todos.....'
                 className='task-input'
-                value={input}
+                // value={zinput}
                 required
                 onChange={onInputChange}
             />
